3d5203c510b4efe7b87a49e01480d4db30afe398
Michael Herman <hermanmu@gmail.com>
2021-07-24 11:41:27 -0500

    updates

--- django_lang/urls.py
+++ django_lang/urls.py
@@ -1,22 +1,10 @@
-"""django_lang URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
+from django.utils.translation import gettext_lazy as _
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+urlpatterns = i18n_patterns(
+    path(_('admin/'), admin.site.urls),
+    path('rosetta/', include('rosetta.urls')),
     path('', include('course.urls')),
-]
+)
